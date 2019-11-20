ba44ea6531ae584d5ef057a3a20b7cf03a296850
atlee19 <atleegraham14@gmail.com>
2019-11-19 22:47:02 -0500

    updating LL test program

--- LinkedStackTest.cpp
+++ LinkedStackTest.cpp
@@ -28,6 +28,7 @@ int main()
         cout << stack.top() << endl;	
         stack.pop();
     }
+    cout << endl;
 
     stack = newStack; //refill
 
@@ -43,45 +44,14 @@ int main()
         cout << newStack.top() << endl;	
         newStack.pop();
     }
+    cout << endl;
 
+    cout << "checking if stack is unchanged" << endl;
+    cout << "elements in stack: " <<endl;
+     while (!stack.isEmptyStack())
+    {
+        cout << stack.top() << endl;	
+        stack.pop();
+    }
 
-//     cout << "After the assignment operator, newStack: " 
-//          << endl; 
-
-//         //Output the elements of newStack
-//     while (!newStack.isEmptyStack())
-//     {
-//         cout << newStack.top() << endl;	
-//         newStack.pop();
-//     }
-
-//         //Use the assignment operator to copy the elements
-//         //of stack into otherStack
-//     otherStack = stack;
-
-//     cout << "Testing the copy constructor." << endl;
-
-//     testCopy(otherStack);
-
-//     cout << "After the copy constructor, otherStack: " << endl;
-
-//     while (!otherStack.isEmptyStack())
-//     {
-//         cout << otherStack.top() << endl;	
-//         otherStack.pop();
-//     }
-
-//     return 0;
-// }
-  
-//      //Function to test the copy constructor
-// void testCopy(linkedStackType<int> OStack) 
-// {
-//     cout << "Stack in the function testCopy:" << endl;
-
-//     while (!OStack.isEmptyStack())
-//     {
-//         cout << OStack.top() << endl;	
-//         OStack.pop();
-//     }
 }
